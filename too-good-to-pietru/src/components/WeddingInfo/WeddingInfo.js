ae4a8aff34f1dd94c211ce419d259b1087edf472
Samantha Farley <samanthafarley32@gmail.com>
2025-02-02 17:11:01 -0500

    added placeholders for wedding info

--- too-good-to-pietru/src/components/WeddingInfo/WeddingInfo.js
+++ too-good-to-pietru/src/components/WeddingInfo/WeddingInfo.js
@@ -7,9 +7,37 @@ const WeddingInfo = () => {
       <div className="headingContainer">
         <div className="headingContent">
           <h1>Wedding Information</h1>
-          <p>This is where we share the plan.</p>
+          <p>Check out the plan for our day here!</p>
         </div>
       </div>
+      <section className="infoBlockContainer">
+        <div className="infoBlock">
+          <h2>Ceremony and Reception</h2>
+          <p>[Name of hotel] Hotel</p>
+          <p>[Hotel address line 1]</p>
+          <p>[Hotel address line 2]</p>
+        </div>
+        <div className="infoBlock">
+          <h2>Directions</h2>
+          <p>Google maps iFrame to location here.</p>
+        </div>
+        <div className="infoBlock">
+          <h2>Accommodations</h2>
+          <p>Hotel + others nearby.</p>
+        </div>
+        <div className="infoBlock">
+          <h2>Bridal Party</h2>
+          <p>Grid of folks in the bridal party here.</p>
+        </div>
+        <div className="infoBlock">
+          <h2>Groomsmen</h2>
+          <p>Grid of folks in the bridal party here.</p>
+        </div>
+        <div className="infoBlock">
+          <h2>Extra Help</h2>
+          <p>Grid of Madi, Livy and Amelia here.</p>
+        </div>
+      </section>
     </div>
   );
 };

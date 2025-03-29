0741286d9995a25cb3cec45829f154809bd4a88e
Samantha Farley <samanthafarley32@gmail.com>
2025-03-29 16:48:32 -0400

    update favicon and tab text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,12 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="footer">
-      <p>&copy; Copyright 2025 Samantha Farley</p>
+      <h4>&copy; Copyright 2025 Samantha Farley</h4>
+      <span className="attribution">
+        <a href="https://www.flaticon.com/free-icons/marry" title="marry icons">
+          Marry icons by mikan933 - Flaticon
+        </a>
+      </span>
     </div>
   );
 };
